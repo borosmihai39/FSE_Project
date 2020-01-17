c33f44bc8d5e4b90e82a496d0fa983400d1a179e
borosmihai39 <borosmihai39@gmail.com>
2020-01-17 20:07:27 +0200

    added new version

--- Assets/scripts/carController.cs
+++ Assets/scripts/carController.cs
@@ -8,21 +8,91 @@ public class carController : MonoBehaviour
 	Vector3 position;
 	public float minPos;
 	public float maxPos = 2.0f;
+	public uiManager ui;
+	bool currentPlatformAndroid = false;
 
+	Rigidbody2D rb;
+
+	void Awake(){
+
+		rb = GetComponent<Rigidbody2D> ();
+
+		#if UNITY_ANDROID
+		currentPlatformAndroid = true;
+		#else
+		currentPlatformAndroid = false;
+		#endif
+
+	}
 
     // Start is called before the first frame update
     void Start()
     {
+		//ui = GetComponent<uiManager> ();
 		position = transform.position;
+
+		if (currentPlatformAndroid) {
+			Debug.Log ("Android");
+		} else {
+			Debug.Log ("Windows");
+		}
     }
 
     // Update is called once per frame
     void Update()
     {
+		if (currentPlatformAndroid) {
+			// android specific codes
+			//TouchMove ();
+			AccelerometerMove ();
+		} else {
+
 		position.x += Input.GetAxis ("Horizontal") * carSpeed * Time.deltaTime;
 
-		position.x = Mathf.Clamp (position.x, -2.0f,2.0f);
+		
 
 		transform.position = position;
+		}
+
+		position = transform.position;
+		position.x = Mathf.Clamp (position.x, -2.4f,2.1f);
+		transform.position = position;
+
+
     }
+
+	// this method is called when two game object get collided
+	void OnCollisionEnter2D (Collision2D col){
+		if (col.gameObject.tag == "Enemy Car") {
+			Destroy (gameObject);
+			//gameObject.SetActive (false);
+			ui.gameOverActivated ();
+			//am.carSound.Stop ();
+		}
+	}
+
+	void AccelerometerMove(){
+		float x = Input.acceleration.x;
+
+		if (x < -0.1f) {
+			MoveLeft ();
+		} else if (x > 0.1f) {
+			MoveRight ();
+		} else {
+			SetVelocityZero ();
+		}
+
+	}
+
+	public void MoveLeft(){
+		rb.velocity = new Vector2 (-carSpeed, 0);
+	}
+
+	public void MoveRight(){
+		rb.velocity = new Vector2 (carSpeed, 0);
+	}
+
+	public void SetVelocityZero(){
+		rb.velocity = Vector2.zero;
+	}
 }
